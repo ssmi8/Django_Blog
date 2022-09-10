ecda007391f454571659058983a9676218264cbc
ssmi8 <smithsteve8@hotmail.com>
2022-09-10 13:53:23 +0000

    Update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-blog-ssmi.heroukapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-blog-ssmi.herokuapp.com', 'localhost']
 
 
 # Application definition
